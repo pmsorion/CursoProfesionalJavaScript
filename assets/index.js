addf4183e91d0322259b4a745de4892925cdf558
Francisco Garcia <garciafran@hotmail.com>
2020-05-07 11:58:12 -0500

    El primer plugin

--- assets/index.js
+++ assets/index.js
@@ -1,16 +1,28 @@
 import MediaPlayer from './MediaPlayer.js';
+import AutoPlay from './plugins/AutoPlay.js';
 
 const video = document.querySelector('video');
-const button = document.querySelector('button');
-const player = new MediaPlayer({ el: video });
+const player = new MediaPlayer({ el: video, plugins: [new AutoPlay()] });
 
-let valor = true
-const changeName = () => {
-    let boton = document.getElementById('playButton')
-    valor ? boton.innerText = 'Pause' : boton.innerText = 'Play';
-    valor = !valor;
+const playButton = document.querySelector('#playButton');
+playButton.onclick = () => player.togglePlay();
+
+const muteButton = document.querySelector('#muteButton');
+muteButton.onclick = () => {
+    player.media.muted ? player.unmute() : player.mute()
 }
 
-button.onclick = () => {
-    player.media.paused ? (player.play(), changeName()) : (player.pause(), changeName())
-}
+
+// let valor = true
+// const changeName = () => {
+//     let boton = document.getElementById('playButton')
+//     valor ? boton.innerText = 'Pause' : boton.innerText = 'Play';
+//     valor = !valor;
+// }
+
+// playButton.onclick = () => {
+//     player.media.paused ? (player.play(), changeName()) : (player.pause(), changeName())
+// }
+
+
+
