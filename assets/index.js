3f514f029cf3d2e57e1ab1722cf8114b6a0d2678
Francisco Garcia <garciafran@hotmail.com>
2020-05-06 21:36:38 -0500

    Scope

--- assets/index.js
+++ assets/index.js
@@ -1,24 +1,11 @@
+import MediaPlayer from './MediaPlayer.js';
+
 const video = document.querySelector('video');
 const button = document.querySelector('button');
-
-
-function MediaPlayer(config) {
-    this.media = config.el
-}
-    
-MediaPlayer.prototype.play = function() {
-    this.media.play();
-}
-
-MediaPlayer.prototype.pause = function() {
-    this.media.pause();
-}
-
 const player = new MediaPlayer({ el: video });
 
 let valor = true
 const changeName = () => {
-    player.play
     let boton = document.getElementById('playButton')
     valor ? boton.innerText = 'Pause' : boton.innerText = 'Play';
     valor = !valor;
